8fe031ec63652f00f23bb72979ebfad2e026caa1
Wojciech Sojka <127860415+sojusz@users.noreply.github.com>
2026-08-04 17:29:52 +0200

    Refactor chart functions and add customer revenue chart
    Refactor chart functions to use a common charting function and add customers revenue chart.

--- python/analysis.py
+++ python/analysis.py
@@ -29,6 +29,30 @@ def chart_to_png(file_name):
         plt.savefig(full_path, dpi=300, bbox_inches="tight")
 
 
+def chart(table, column, width, title, xlabel, ylabel, png_name, chart_name):
+    plt.figure(figsize=(width, 6))
+    if chart_name == "bar":
+        plt.barh(table[column][::-1], table["revenue"][::-1] / 1000, color="skyblue")
+    elif chart_name == "basic":
+        plt.plot(table.index, table.values, marker="o", color="b", linestyle="-")
+
+    plt.title(title, fontsize=14)
+    plt.xlabel(xlabel, fontsize=12)
+    plt.ylabel(ylabel, fontsize=12)
+    if chart_name == "bar":
+        plt.grid(axis="x", linestyle="--", alpha=0.6)
+        for index, value in enumerate(table["revenue"][::-1] / 1000):
+            plt.text(value + 0.5, index, f"${value:.1f}k", va="center", fontsize=9)
+
+        plt.tight_layout()
+    elif chart_name == "basic":
+        plt.xticks(table.index)
+        plt.grid(True, linestyle="--", alpha=0.6)
+
+    chart_to_png(png_name)
+    plt.show()
+
+
 def monthly_revenue_chart():
     orders["orderDate"] = pd.to_datetime(orders["orderDate"])
     df = pd.merge(orders, orderdetails, on="orderNumber")
@@ -36,17 +60,8 @@ def monthly_revenue_chart():
     df_2005 = df[df["orderDate"].dt.year == 2005].copy()
     monthly_revenue = df_2005.groupby(df_2005["orderDate"].dt.month)['totalValue'].sum()
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(monthly_revenue.index, monthly_revenue.values, marker="o", color="b", linestyle="-")
-
-    plt.title("Total monthly revenue for 2005", fontsize=14)
-    plt.xlabel("Month", fontsize=12)
-    plt.ylabel("Revenue (USD)", fontsize=12)
-
-    plt.xticks(monthly_revenue.index)
-    plt.grid(True, linestyle="--", alpha=0.6)
-    chart_to_png("monthly_revenue.png")
-    plt.show()
+    chart(monthly_revenue, "", 10, "Total monthly revenue for 2005", "Month", "Revenue (USD)",
+          "monthly_revenue.png", "basic")
 
 
 def top_products_chart():
@@ -64,20 +79,8 @@ def top_products_chart():
         .sum().reset_index().sort_values(by="revenue", ascending=False).head(10)
     )
 
-    plt.figure(figsize=(15, 6))
-    plt.barh(top10_products["productName"][::-1], top10_products["revenue"][::-1] / 1000, color="skyblue")
-
-    plt.title("Top 10 products last year")
-    plt.xlabel("Revenue (in thousand USD)", fontsize=12)
-    plt.ylabel("Product's name", fontsize=12)
-    plt.grid(axis="x", linestyle="--", alpha=0.6)
-
-    for index, value in enumerate(top10_products["revenue"][::-1] / 1000):
-        plt.text(value + 0.5, index, f"${value:.1f}k", va="center", fontsize=9)
-
-    plt.tight_layout()
-    chart_to_png("top_products.png")
-    plt.show()
+    chart(top10_products, "productName", 15, "Top 10 products last year", "Revenue (in thousand USD)",
+          "Product's name", "top_products.png", "bar")
 
 
 def categories_revenue_chart():
@@ -90,27 +93,42 @@ def categories_revenue_chart():
     df_last_year = df[df["orderDate"].dt.year == last_year].copy()
     df_last_year["revenue"] = df_last_year["quantityOrdered"] * df_last_year["priceEach"]
 
-    categories = (
-        df_last_year.groupby(df_last_year["productLine"])["revenue"]
+    categories_list = (
+        df_last_year.groupby("productLine")["revenue"]
         .sum().reset_index().sort_values(by="revenue", ascending=False)
     )
 
-    plt.figure(figsize=(12, 6))
-    plt.barh(categories["productLine"][::-1], categories["revenue"][::-1] / 1000, color="red")
+    categories_list["share"] = categories_list["revenue"] / categories_list["revenue"].sum() * 100
+    print(categories_list)
 
-    plt.title("Categories revenue last year")
-    plt.xlabel("Revenue (in thousand USD)")
-    plt.ylabel("Categories names")
-    plt.grid(axis="x", linestyle="--", alpha=0.6)
+    chart(categories_list, "productLine", 13, "Categories revenue last year", "Revenue (in thousand USD)",
+          "Categories names", "categories_revenue.png", "bar")
 
-    for index, value in enumerate(categories["revenue"][::-1] / 1000):
-        plt.text(value + 0.5, index, f"${value:.1f}k", va="center", fontsize=9)
 
-    plt.tight_layout()
-    chart_to_png("categories_revenue.png")
-    plt.show()
+def customers_revenue_chart():
+    orders["orderDate"] = pd.to_datetime(orders["orderDate"])
+    last_year = orders["orderDate"].dt.year.max()
 
+    df = pd.merge(customers, orders, on="customerNumber")
+    df = pd.merge(df, orderdetails, on="orderNumber")
+
+    df_last_year = df[df["orderDate"].dt.year == last_year].copy()
+    df_last_year["revenue"] = df_last_year["quantityOrdered"] * df_last_year["priceEach"]
+
+    top5_customers = (
+        df_last_year.groupby("customerName")["revenue"]
+        .sum().reset_index().sort_values(by="revenue", ascending=False).head(5)
+    )
+
+    all_rev = df_last_year["revenue"].sum()
+
+    print(top5_customers["revenue"].sum() / all_rev * 100)
+
+    chart(top5_customers, "customerName", 14, "Top 5 customers", "Revenue (in thousand USD)",
+          "Client names", "top_customers.png", "bar")
 
 # monthly_revenue_chart()
 # top_products_chart()
 # categories_revenue_chart()
+# customers_revenue_chart()
+# df_info(customers)
